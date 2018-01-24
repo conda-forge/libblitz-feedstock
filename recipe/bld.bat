87ddcfa997342ec5c055ed3f379f0e72047efd50
Ali Khodabakhsh <ali.khodabakhsh@ntnu.no>
2018-01-24 20:24:25 +0100

    debug

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,4 @@
 cd %SRC_DIR%
-rem C:\cygwin\bin\unzip Blitz-VS2005.NET.zip
-rem powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('Blitz-VS2005.NET.zip', ''); }"
-rem cd Blitz-VS2005.NET
-
 7z x Blitz-VS2010.zip -aoa
 
 set SLN_FILE=Blitz-Library.sln
@@ -10,6 +6,13 @@ set SLN_CFG=Release
 if "%ARCH%"=="32" (set SLN_PLAT=Win32) else (set SLN_PLAT=x64)
 
 REM Build step
-rem msbuild "%SLN_FILE%"
-devenv "%SLN_FILE%" /Build "%SLN_CFG%|%SLN_PLAT%"
+msbuild "%SLN_FILE%"
 if errorlevel 1 exit 1
+
+rem ---------------------
+
+rem C:\cygwin\bin\unzip Blitz-VS2005.NET.zip
+rem powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('Blitz-VS2005.NET.zip', ''); }"
+rem cd Blitz-VS2005.NET
+
+rem devenv "%SLN_FILE%" /Build "%SLN_CFG%|%SLN_PLAT%"
