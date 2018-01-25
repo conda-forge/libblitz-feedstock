a0608ac08c494cad87a522e99493eff674d13e60
alikhoda <ali.khodabakhsh@gmail.com>
2018-01-25 21:55:01 +0100

    add move

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,12 +7,8 @@ if "%ARCH%"=="32" (set SLN_PLAT=Win32) else (set SLN_PLAT=x64)
 
 REM Build step
 msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140
+msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140,ConfigurationType=DynamicLibrary
 if errorlevel 1 exit 1
 
-rem  ---------------------
-
-rem C:\cygwin\bin\unzip Blitz-VS2005.NET.zip
-rem powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('Blitz-VS2005.NET.zip', ''); }"
-rem cd Blitz-VS2005.NET
-
-rem devenv "%SLN_FILE%" /Build "%SLN_CFG%|%SLN_PLAT%"
+move %SRC_DIR%\lib\%ARCH%\blitz.dll %LIBRARY_BIN%
+move %SRC_DIR%\lib\%ARCH%\blitz.lib %LIBRARY_LIB%
