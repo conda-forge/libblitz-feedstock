a31bb60fe8271d045615c50cbc81560449f8799c
alikhoda <ali.khodabakhsh@gmail.com>
2018-01-25 22:36:36 +0100

    rem py27

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,5 +10,5 @@ msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformTool
 msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140,ConfigurationType=DynamicLibrary
 if errorlevel 1 exit 1
 
-move %SRC_DIR%\lib\%SLN_PLAT%\blitz.dll %LIBRARY_BIN%
-move %SRC_DIR%\lib\%SLN_PLAT%\blitz.lib %LIBRARY_LIB%
+copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.dll %LIBRARY_BIN%\
+copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.lib %LIBRARY_LIB%\
