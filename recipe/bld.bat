900bdc8da060cff2aac724de68a5786b97ffc211
alikhoda <ali.khodabakhsh@gmail.com>
2018-01-28 18:36:32 +0100

    add pkgcfg

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,3 +12,19 @@ if errorlevel 1 exit 1
 
 copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.dll %LIBRARY_BIN%\
 copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.lib %LIBRARY_LIB%\
+
+mkdir %LIBRARY_LIB%\pkgconfig
+
+(
+@echo prefix=%CONDA_PREFIX:\=/%/Conda/Library
+@echo exec_prefix=${prefix}
+@echo libdir=${exec_prefix}/lib
+@echo includedir=${prefix}/include
+@echo.
+@echo Name: blitz
+@echo Description: blitz Library
+@echo Version: 0.10
+@echo Requires: 
+@echo Libs: -L${libdir} -lblitz
+@echo Cflags: -I${includedir} -pthread
+) > %LIBRARY_LIB%\pkgconfig\blitz.pc
