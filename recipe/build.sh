9bb047f74bb4ea0808b0988c5404f750fb82bec9
Amir Mohammadi <5738695+183amir@users.noreply.github.com>
2021-09-30 20:47:53 +0200

    Add common cmake options

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -ex
 srcdir=$SRC_DIR
 mkdir build
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS=ON
 make lib
 make install
