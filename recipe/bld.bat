8616db018f6ae43365d97463048487f8f26c83a3
Amir Mohammadi <5738695+183amir@users.noreply.github.com>
2021-09-30 20:38:03 +0200

    use cmake to build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,36 +1,16 @@
 cd %SRC_DIR%
-7z x Blitz-VS2010.zip -aoa
 
-set SLN_FILE=Blitz-Library.sln
-set SLN_CFG=Release
-if "%ARCH%"=="32" (set SLN_PLAT=Win32) else (set SLN_PLAT=x64)
+md build
+cd build
 
-REM Build step
-msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140
-msbuild "%SLN_FILE%" /p:Configuration=%SLN_CFG%,Platform=%SLN_PLAT%,PlatformToolset=v140,ConfigurationType=DynamicLibrary
-if errorlevel 1 exit 1
+cmake .. -DCMAKE_GENERATOR_PLATFORM="x64" -DCMAKE_BUILD_TYPE=Release -DPython3_EXECUTABLE="%PYTHON%"
+if errorlevel 1 exit /b 1
 
-copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.dll %LIBRARY_BIN%\
-copy %SRC_DIR%\lib\%SLN_PLAT%\blitz.lib %LIBRARY_LIB%\
+cmake --build . --config Release
+if errorlevel 1 exit /b 1
 
-REM Copy headers
-mkdir %LIBRARY_INC%\blitz
-mkdir %LIBRARY_INC%\blitz\array
-mkdir %LIBRARY_INC%\blitz\gnu
-mkdir %LIBRARY_INC%\blitz\meta
-mkdir %LIBRARY_INC%\blitz\ms
-
-copy %SRC_DIR%\blitz\*.h %LIBRARY_INC%\blitz\
-copy %SRC_DIR%\blitz\array\*.h %LIBRARY_INC%\blitz\array\
-copy %SRC_DIR%\blitz\gnu\*.h %LIBRARY_INC%\blitz\gnu\
-copy %SRC_DIR%\blitz\meta\*.h %LIBRARY_INC%\blitz\meta\
-copy %SRC_DIR%\blitz\ms\*.h %LIBRARY_INC%\blitz\ms\
-
-copy %SRC_DIR%\blitz\*.cc %LIBRARY_INC%\blitz\
-copy %SRC_DIR%\blitz\array\*.cc %LIBRARY_INC%\blitz\array\
-copy %SRC_DIR%\blitz\gnu\*.cc %LIBRARY_INC%\blitz\gnu\
-copy %SRC_DIR%\blitz\meta\*.cc %LIBRARY_INC%\blitz\meta\
-copy %SRC_DIR%\blitz\ms\*.cc %LIBRARY_INC%\blitz\ms\
+cmake --build . --target install
+if errorlevel 1 exit /b 1
 
 REM make pkg-config file
 mkdir %LIBRARY_LIB%\pkgconfig
@@ -44,7 +24,7 @@ mkdir %LIBRARY_LIB%\pkgconfig
 @echo Name: blitz
 @echo Description: blitz Library
 @echo Version: 0.10
-@echo Requires: 
+@echo Requires:
 @echo Libs: -L${libdir} -lblitz
 @echo Cflags: -I${includedir} -pthread
 ) > %LIBRARY_LIB%\pkgconfig\blitz.pc
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-
-if [[ $ARCH == 64 ]];
-then
-	export ARCH_CMD="--enable-64bit"
-else
-	export ARCH_CMD="--enable-simd-width=8"
-fi
-
-./configure \
-  --prefix="${PREFIX}" \
-  --enable-static \
-  --enable-shared \
-  --enable-optimize \
-  --enable-threadsafe \
-  ${ARCH_CMD} \
-  CPPFLAGS="-I${PREFIX}/include -pthread" \
-  CFLAGS="-I${PREFIX}/include -pthread -DBZHAVE_STD" \
-  CXXFLAGS="-I${PREFIX}/include -pthread -DBZHAVE_STD" \
-  LDFLAGS="-L${PREFIX}/lib"
-make
-make check
+srcdir=$SRC_DIR
+mkdir build
+cd build
+cmake ..
+make lib
 make install
