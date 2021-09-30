48663f4e872f6c7b308b4701537bda38174e131f
Amir Mohammadi <5738695+183amir@users.noreply.github.com>
2021-09-30 22:47:57 +0200

    remve the check for blitz.dll

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,20 +11,3 @@ if errorlevel 1 exit /b 1
 
 cmake --build . --target install
 if errorlevel 1 exit /b 1
-
-REM make pkg-config file
-mkdir %LIBRARY_LIB%\pkgconfig
-
-(
-@echo prefix=%CONDA_PREFIX:\=/%/Conda/Library
-@echo exec_prefix=${prefix}
-@echo libdir=${exec_prefix}/lib
-@echo includedir=${prefix}/include
-@echo.
-@echo Name: blitz
-@echo Description: blitz Library
-@echo Version: 0.10
-@echo Requires:
-@echo Libs: -L${libdir} -lblitz
-@echo Cflags: -I${includedir} -pthread
-) > %LIBRARY_LIB%\pkgconfig\blitz.pc
