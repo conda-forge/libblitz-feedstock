71e2eb10cb5be6faa779ff8783813f3861b212f0
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2021-10-12 22:42:18 +0200

    Remove --enable-optimize for disabling -ffast-math See https://github.com/conda-forge/ctng-compiler-activation-feedstock/issues/57#issuecomment-941439534

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,11 +14,11 @@ else
 	export ARCH_CMD="--enable-simd-width=8"
 fi
 
+# --enable-optimize breaks Tensorflow, see https://github.com/conda-forge/ctng-compiler-activation-feedstock/issues/57#issuecomment-941439534
 ./configure \
   --prefix="${PREFIX}" \
   --enable-static \
   --enable-shared \
-  --enable-optimize \
   --enable-threadsafe \
   --enable-serialization \
   ${ARCH_CMD} \
